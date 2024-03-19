b7ef63fd9249b8baf96ce13e626370c7dbff355e
AsynchronousAI <aarav.sethi@outlook.com>
2024-03-18 21:14:32 -0500

    Binary operations

--- main.py
+++ main.py
@@ -16,6 +16,57 @@ def visit(node,nested=False):
     elif type == 'AstExprError':
         print("Syntax error")
         exit(1)
+    elif type == 'AstExprBinary':
+        op = None
+        if node['op'] == 'Add':
+            op = ast.Add()
+        elif node['op'] == 'Sub':
+            op = ast.Sub()
+        elif node['op'] == 'Mul':
+            op = ast.Mult()
+        elif node['op'] == 'Div':
+            op = ast.Div()
+        elif node['op'] == 'Mod':
+            op = ast.Mod()
+        elif node['op'] == 'Pow':
+            op = ast.Pow()
+        elif node['op'] == 'Concat':
+            op = ast.Add()
+            if node['left']['type'] != 'AstExprConstantString':
+                node['left']['type'] = 'AstExprConstantString'
+                node['left']['value'] = str(node['left']['value'])
+            if node['right']['type'] != 'AstExprConstantString':
+                node['right']['type'] = 'AstExprConstantString'
+                node['right']['value'] = str(node['right']['value'])
+        elif node['op'] == 'Eq':
+            op = ast.Eq()
+        elif node['op'] == 'Ne':
+            op = ast.NotEq()
+        elif node['op'] == 'Lt':
+            op = ast.Lt()
+        elif node['op'] == 'Le':
+            op = ast.LtE()
+        elif node['op'] == 'Gt':
+            op = ast.Gt()
+        elif node['op'] == 'Ge':
+            op = ast.GtE()
+        elif node['op'] == 'And':
+            op = ast.And()
+        elif node['op'] == 'Or':
+            op = ast.Or()
+        else:
+            print(f"Unhandled operator: {node['op']}")
+            exit(1)
+
+        if nested:
+            return ast.BinOp(left=visit(node['left'], True), op=op, right=visit(node['right'], True))
+        else:
+            tree.append(ast.BinOp(left=visit(node['left'], True), op=op, right=visit(node['right'], True)))
+    elif type == 'AstExprConstantNumber':
+        if nested:
+            return ast.Constant(n=node['value'])
+        else:
+            tree.append(ast.Constant(n=node['value']))
     elif type == 'AstStatExpr':
         tree.append(ast.Expr(value=visit(node['expr'], True)))
     elif type == 'AstExprCall':
